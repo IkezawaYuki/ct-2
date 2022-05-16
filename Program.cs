602518c46d49aa6acf6b1c9fe3e7b8e704120a05
IkezawaYuki <y.ikezawa93@gmail.com>
2022-05-16 23:29:25 +0900

    mod

--- Program.cs
+++ Program.cs
@@ -11,7 +11,7 @@ namespace TrackerUI
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
             ApplicationConfiguration.Initialize();
-            TrackerLibrary.GlobalConfig.InitializeConnections(true, true);
+            TrackerLibrary.GlobalConfig.InitializeConnections(TrackerLibrary.DatabaseType.Sql);
             Application.Run(new TournamentDashboardForm());
         }
     }
